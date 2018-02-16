fa14d38eabf05746bf40104082b16956d1092abd
Anna Nixon <annapnixon@gmail.com>
2018-02-15 18:18:19 -0800

    Changed robot map needs testing

--- robot/src/main/kotlin/com/team1510/robot/config/RobotMap.kt
+++ robot/src/main/kotlin/com/team1510/robot/config/RobotMap.kt
@@ -8,8 +8,8 @@ const val RIGHT_SLAVE_CANID: Int = 5
 //Bunnybot: 8 3 4 5
 //1510: 1,4,2,5
 //Ramp:
-const val RIGHT_RAMP_SOL_FWD_ID: Int = 2
-const val RIGHT_RAMP_SOL_REV_ID: Int = 3
+const val RIGHT_RAMP_SOL_FWD_ID: Int = 0
+const val RIGHT_RAMP_SOL_REV_ID: Int = 1
 
 const val LEFT_RAMP_SOL_FWD_ID: Int = 4
 const val LEFT_RAMP_SOL_REV_ID: Int = 5
@@ -21,8 +21,8 @@ const val LEFT_RAMP_SOL_REV_ID: Int = 5
 //const val LEFT_LOCK_SOL_REV_ID: Int = 8
 
 //Intake:
-const val INTAKE_SOL_FWD_ID: Int = 0
-const val INTAKE_SOL_REV_ID: Int = 1
+const val INTAKE_SOL_FWD_ID: Int = 2
+const val INTAKE_SOL_REV_ID: Int = 3
 
 const val LEFT_INTAKE_SOL_FWD_ID: Int = 11
 const val LEFT_INTAKE_SOL_REV_ID: Int = 12
