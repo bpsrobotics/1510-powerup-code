36e15f54184edf934b951b58e986bd798b2630b0
baoanhtdn <baoanhtdn@gmail.com>
2018-02-16 17:34:20 -0800

    Some more autonomous code

--- robot/src/main/kotlin/com/team1510/robot/commands/Autonomous.kt
+++ robot/src/main/kotlin/com/team1510/robot/commands/Autonomous.kt
@@ -2,12 +2,13 @@ package com.team1510.robot.commands
 
 import com.team1510.robot.subsystems.Arm
 import com.team1510.robot.subsystems.Drivetrain
+import com.team1510.robot.subsystems.Intake
 import edu.wpi.first.wpilibj.command.Command
 
 class Autonomous : Command() {
 
     override fun initialize() {
-
+        Intake.intakeRetract()
     }
 
     override fun execute() {
@@ -16,7 +17,12 @@ class Autonomous : Command() {
             if(Drivetrain.gameInfo.get(0) == 'L') {
                 Drivetrain.moveDistance(100.0, 100.0)
                 Drivetrain.moveDistance(-50.0, 50.0)
-                Drivetrain.moveDistance(-50.0, 50.0)
+                Drivetrain.moveDistance(86.75, 86.75)
+                Drivetrain.moveDistance(96.0, 96.0)
+                Drivetrain.moveDistance(9.0, 9.0)
+                Intake.updateIntake(false, true)//first cube to switch
+                Intake.intakeExtend()
+
             }
         }
     }
