a189faddb1a936e89a44a343d67ca2a79f11e1b0
Anna Nixon <annapnixon@gmail.com>
2018-02-25 20:58:10 -0800

    Added selection to SmartDashboard

--- robot/src/main/kotlin/com/team1510/robot/Robot.kt
+++ robot/src/main/kotlin/com/team1510/robot/Robot.kt
@@ -1,14 +1,14 @@
 package com.team1510.robot
 
-import com.team1510.robot.commands.Autonomous
-import com.team1510.robot.commands.PIDTeleop
+import com.team1510.robot.commands.*
 import com.team2898.engine.logic.LoopManager
 import edu.wpi.first.wpilibj.IterativeRobot
 import edu.wpi.first.wpilibj.command.Scheduler
 import com.team1510.robot.subsystems.DrivePIDTest
-import com.team1510.robot.commands.Teleop
 //import com.team1510.robot.subsystems.Arm
 import edu.wpi.first.wpilibj.CameraServer
+import edu.wpi.first.wpilibj.smartdashboard.*
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
 import edu.wpi.first.wpilibj.networktables.NetworkTable as nt
 
 class Robot : IterativeRobot() {
@@ -16,10 +16,22 @@ class Robot : IterativeRobot() {
     val autoCommand = Autonomous()
 
     val teleopCommand = Teleop()
+
+    val autoChooser = SendableChooser()
+
+
+
     override fun robotInit() {
         //Arm.updatePower(0.0)
         CameraServer.getInstance().startAutomaticCapture(0)
         CameraServer.getInstance().startAutomaticCapture(1)
+
+        autoChooser.addDefault("CrossLine", CrossLine())
+        autoChooser.addObject("Center Switch", RightSwitch())
+        autoChooser.addObject("Right Switch", RightSwitch())
+        autoChooser.addObject("Left Switch", RightSwitch())
+
+        SmartDashboard.putData("Auto Chooser", autoChooser)
     }
 
     override fun autonomousInit() {
